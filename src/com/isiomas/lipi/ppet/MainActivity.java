56d2ae0cd48908c5e01744709752654a91975159
Iqbal Syamsu <iqbalsyamsu@gmail.com>
2015-03-16 20:20:30 +0700

    tess
    tessss

--- src/com/isiomas/lipi/ppet/MainActivity.java
+++ src/com/isiomas/lipi/ppet/MainActivity.java
@@ -4,14 +4,42 @@ import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.text.Html;
 
 public class MainActivity extends Activity
 {
+	private final String htmlText = "<body><h1>Heading Text</h1><p>This tutorial " +
+            "explains how to display " +
+            "<strong>HTML </strong>text in android text view.&nbsp;</p>" +
+            "<img src=\"hughjackman.jpg\">" +
+            "<blockquote>Example from <a href=\"www.javatechig.com\">" +
+            "Javatechig.com<a></blockquote></body>";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		TextView htmlTextView = (TextView)findViewById(R.id.html_text);
+		htmlTextView.setText(Html.fromHtml(htmlText, new ImageGetter(), null));
     }
+
+	private class ImageGetter implements Html.ImageGetter {
+
+	public Drawable getDrawable(String source) {
+		int id;
+		if (source.equals("hughjackman.jpg")) {
+			id = R.drawable.hughjackman;
+		}
+		else {
+			return null;
+        }
+
+		Drawable d = getResources().getDrawable(id);
+		d.setBounds(0,0,d.getIntrinsicWidth(),d.getIntrinsicHeight());
+		return d;
+	}
+	};
 }
