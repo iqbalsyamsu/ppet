788b22eb95930a050d75b33246ec8d2378c04bb1
Iqbal Syamsu <iqbalsyamsu@gmail.com>
2015-03-17 22:01:36 +0700

    gdg

--- src/com/isiomas/lipi/ppet/MainActivity.java
+++ src/com/isiomas/lipi/ppet/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.*;
 import android.text.Html;
 import android.graphics.drawable.Drawable;
 import android.view.View.OnClickListener;
+import android.widget.Button.*;
 
 public class MainActivity extends Activity
 {
